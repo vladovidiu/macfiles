bad8dd35cb992633adabff151bc9cba4bc48f0dd
vladovidiu <vladovidiu.tutunea@gmail.com>
2022-09-13 20:48:48 +0100

    fix transparency for tokyonight

--- config/.config/nvim/after/plugin/tokyonight.lua
+++ config/.config/nvim/after/plugin/tokyonight.lua
@@ -2,10 +2,20 @@ vim.g.tokyonight_transparent_sidebar = true
 vim.g.tokyonight_transparent = true
 vim.g.tokyonight_italic_functions = true
 
+local status_ok, tokyonight = pcall(require, "tokyonight")
+if not status_ok then
+	return
+end
+
+tokyonight.setup({
+	transparent = true,
+})
+
 vim.cmd("colorscheme tokyonight")
 
 vim.cmd([[
     highlight NormalFloat guibg=none
+    highlight MsgArea guibg=none
     highlight Float guibg=none
     highlight Normal guibg=none
     highlight TelescopeNormal guibg=none
