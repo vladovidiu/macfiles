7e40e0d86733109cd2668941bf8e42a3d4fccfbd
vladovidiu <vladovidiu.tutunea@gmail.com>
2022-10-10 14:59:57 +0100

    remove transparency for less distractions

--- config/.config/nvim/after/plugin/tokyonight.lua
+++ config/.config/nvim/after/plugin/tokyonight.lua
@@ -1,5 +1,5 @@
-vim.g.tokyonight_transparent_sidebar = true
-vim.g.tokyonight_transparent = true
+-- vim.g.tokyonight_transparent_sidebar = true
+-- vim.g.tokyonight_transparent = true
 vim.g.tokyonight_italic_functions = true
 
 local status_ok, tokyonight = pcall(require, "tokyonight")
@@ -13,18 +13,18 @@ tokyonight.setup({
 
 vim.cmd("colorscheme tokyonight")
 
-vim.cmd([[
-    highlight NormalFloat guibg=none
-    highlight MsgArea guibg=none
-    highlight Float guibg=none
-    highlight Normal guibg=none
-    highlight TelescopeNormal guibg=none
-    highlight TelescopeBorder guibg=none
-    highlight FloatBorder guibg=none
-    highlight DiagnosticVirtualTextError guibg=none
-    highlight DiagnosticVirtualTextWarn guibg=none
-    highlight DiagnosticVirtualTextInfo guibg=none
-    highlight DiagnosticVirtualTextHint guibg=none
-    highlight LspFloatWinNormal guibg=none
-    highlight CursorLineNr guifg=#bb9af7
-]])
+-- vim.cmd([[
+--     highlight NormalFloat guibg=none
+--     highlight MsgArea guibg=none
+--     highlight Float guibg=none
+--     highlight Normal guibg=none
+--     highlight TelescopeNormal guibg=none
+--     highlight TelescopeBorder guibg=none
+--     highlight FloatBorder guibg=none
+--     highlight DiagnosticVirtualTextError guibg=none
+--     highlight DiagnosticVirtualTextWarn guibg=none
+--     highlight DiagnosticVirtualTextInfo guibg=none
+--     highlight DiagnosticVirtualTextHint guibg=none
+--     highlight LspFloatWinNormal guibg=none
+--     highlight CursorLineNr guifg=#bb9af7
+-- ]])
